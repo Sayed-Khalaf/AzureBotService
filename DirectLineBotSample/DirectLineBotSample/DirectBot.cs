be669959244fe0e69b8ec21b998aa48b098b62b5
Sayed Khalaf <Sayed.Khalaf@emeint.net>
2018-12-26 10:55:19 +0200

    Initial Commit

---
+++ DirectLineBotSample/DirectLineBotSample/DirectBot.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace DirectLineBotSample
+{
+    public class DirectBot : IBot
+    {
+        public Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Respond to the various activity types.
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // Respond to the incoming text message.
+                    RespondToMessage(context);
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    break;
+
+                case ActivityTypes.ContactRelationUpdate:
+                    break;
+
+                case ActivityTypes.Typing:
+                    break;
+
+                case ActivityTypes.DeleteUserData:
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Responds to the incoming message by either sending a hero card, an image, 
+        /// or echoing the user's message.
+        /// </summary>
+        /// <param name="context">The context of this conversation.</param>
+        private void RespondToMessage(ITurnContext context)
+        {
+            switch (context.Activity.Text.Trim().ToLower())
+            {
+                case "hi":
+                case "hello":
+                case "help":
+                    // Send the user an instruction message.
+                    context.SendActivityAsync("Welcome DirectLine API Sample. " +
+                    "Send \"Send me hero card\" or \"Send me image\" "+
+                    ". Any other message will be echoed.");
+                    break;
+
+                case "send me hero card":
+                    // Create the hero card.
+                    HeroCard heroCard = new HeroCard()
+                    {
+                        Title = "Sample Hero Card",
+                        Text = "Displayed in the DirectLine client"
+                    };
+
+                    // Attach the hero card to a new activity.
+                    context.SendActivityAsync(MessageFactory.Attachment(heroCard.ToAttachment()));
+                    break;
+
+                case "send me image":
+                    // Create the image attachment.
+                    Attachment imageAttachment = new Attachment()
+                    {
+                        ContentType = "image/png",
+                        ContentUrl = "https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png",
+                    };
+
+                    // Attach the image attachment to a new activity.
+                    context.SendActivityAsync(MessageFactory.Attachment(imageAttachment));
+                    break;
+
+                default:
+                    // No command was encountered. Echo the user's message.
+                    context.SendActivityAsync($"You said \"{context.Activity.Text}\"");
+                    break;
+            }
+        }
+    }
+}
